187b7e613aa8e5d7b8a0d7d3440028dc701603a6
Kadek Rama Artha Mahesa <rama.artha131@student.unud.ac.id>
2024-12-08 22:33:41 +0800

    update

--- resources/views/map-latihan1.blade.php
+++ resources/views/map-latihan1.blade.php
@@ -19,6 +19,7 @@
         defer></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 </head>
 
 <body class="bg-gray-100 font-sans">
